935b17e8e2a16b60ddf88364cbc4a21038320a7e
Hiroshi Kikuchi <hiroshi.kikuchi.dvs@diverse-inc.com>
2018-09-21 15:08:43 +0900

    initial commit

---
+++ gcd-repository.go
@@ -0,0 +1,85 @@
+// +build appengine
+
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/labstack/echo"
+	context2 "golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"math/rand"
+	"time"
+)
+
+const KindImage string = "Image"
+
+type gcdRepository struct {
+	c context.Context
+}
+
+
+func (r *gcdRepository) FindImageById(id int64) (*Image, error) {
+	key := datastore.NewKey(r.c, KindImage, "", id, nil)
+	var image Image
+	if err := datastore.Get(r.c, key, &image); err != nil {
+		return nil, err
+	}
+	return &image, nil
+}
+
+func (r *gcdRepository) RandomImageId() (int64, error) {
+	total, err := datastore.NewQuery(KindImage).Count(r.c)
+	if err != nil {
+		return -1, err
+	}
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(total)
+	t := datastore.NewQuery(KindImage).Offset(index).Limit(1).KeysOnly().Run(r.c)
+	key, err := t.Next(nil)
+	if err != nil {
+		return -1, err
+	}
+	return key.IntID(), nil
+}
+
+
+func (r *gcdRepository) LoadImages(count uint, page uint) ([]Image, error) {
+	query := datastore.
+		NewQuery("Image").
+		Limit(int(count)).
+		Offset(int(count * (page - 1))).
+		Order("-CreatedAt").
+		Filter("IsDeleted =", false)
+	var images []Image
+	if _, err := query.GetAll(r.c, &images); err != nil {
+		return nil, err
+	}
+	return images, nil
+}
+
+func (r *gcdRepository) SaveImage(image *Image) error {
+	return datastore.RunInTransaction(r.c, func(tc context2.Context) error {
+		exists, err := datastore.NewQuery(KindImage).Filter("Url =", image.Url).Count(r.c)
+		if err != nil {
+			return err
+		}
+		if exists > 0 {
+			// TODO: Error実装型にして重複時に専用のエラーメッセージ出せるようにしたい
+			return errors.New("Duplicating URL: " + image.Url)
+		}
+		key := datastore.NewIncompleteKey(r.c, KindImage, nil)
+		if _, err := datastore.Put(r.c, key, image); err != nil {
+			return err
+		}
+		return nil
+	}, nil)
+}
+
+func RepositoryGenerator(c echo.Context) Repository {
+	return &gcdRepository{
+		c: appengine.NewContext(c.Request()),
+	}
+}
+
---
+++ users.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"errors"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/microcosm-cc/bluemonday"
+	"google.golang.org/appengine/datastore"
+	"html/template"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var (
+	p = bluemonday.UGCPolicy()
+)
+
+type (
+	Entity struct {
+		CreatedAt time.Time
+		UpdatedAt time.Time
+		DeletedAt time.Time
+		IsDeleted bool
+	}
+	Image struct {
+		Entity
+		Url       string
+		ServerKey *datastore.Key
+	}
+	Server struct {
+		Entity
+		Url string
+	}
+	Repository interface {
+		SaveImage(image *Image) error
+		LoadImages(count uint, page uint) ([]Image, error)
+		RandomImageId() (int64, error)
+		FindImageById(id int64) (*Image, error)
+	}
+	RepositoryGeneratorFunc func(c echo.Context) Repository
+
+	Template struct {
+		templates *template.Template
+	}
+)
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func init() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("template/*.html")),
+	}
+	e.Renderer = t
+
+	// hook into the echo instance to create an endpoint group
+	// and add specific middleware to it plus handlers
+	e.Static("/", "public")
+	e.GET("/browse", showAllImagesGenerator(RepositoryGenerator))
+	e.GET("/g", showRandomImageGenerator(RepositoryGenerator))
+
+	g := e.Group("/images")
+	g.Use(middleware.CORS())
+	g.POST("/", saveImageGenerator(RepositoryGenerator))
+	g.GET("/:id", showImageGenerator(RepositoryGenerator))
+	g.Static("/", "public/images")
+
+	g = e.Group("/console")
+	g.Use(middleware.BasicAuth(func(username string, password string, c echo.Context) (bool, error) {
+		if username == "" || password == "" {
+			return false, nil
+		}
+	}))
+}
+
+func showAllImagesGenerator(rg RepositoryGeneratorFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := rg(c)
+		rawPage := c.QueryParam("rawPage")
+		if rawPage == "" {
+			rawPage = "1"
+		}
+		page, err := strconv.Atoi(rawPage)
+		if err != nil {
+			return err
+		}
+		images, err := r.LoadImages(50, uint(page))
+		if err != nil {
+			return err
+		}
+		return c.Render(http.StatusOK, "browse.template.html", map[string]interface{}{
+			"prev":   page - 1,
+			"next":   page + 1,
+			"images": images,
+		})
+	}
+}
+
+func showRandomImageGenerator(rg RepositoryGeneratorFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := rg(c)
+		id, err := r.RandomImageId()
+		if err != nil {
+			return err
+		}
+		if id < 0 {
+			return errors.New("no images in DB")
+		}
+		return c.Redirect(http.StatusFound, "/images/"+strconv.Itoa(int(id)))
+	}
+}
+
+func saveImageGenerator(rg RepositoryGeneratorFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := rg(c)
+		println("Called")
+		url := p.Sanitize(c.Request().FormValue("image_url"))
+		image := &Image{
+			Entity{
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+				IsDeleted: false,
+			},
+			url,
+			nil,
+		}
+		if err := r.SaveImage(image); err != nil {
+			return err
+		}
+		return c.HTML(http.StatusOK, "<p>done</p>")
+	}
+}
+
+func showImageGenerator(rg RepositoryGeneratorFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := rg(c)
+		rawId := c.Param("id")
+		id, err := strconv.Atoi(rawId)
+		if err != nil {
+			return err
+		}
+		image, err := r.FindImageById(int64(id))
+		return c.Render(http.StatusOK, "image.detail.template.html", image)
+	}
+}
